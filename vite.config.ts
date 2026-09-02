6720b5e90870532078920426d0dcc91a0131a46e
nikskr <nskrabnevsky@mail.ru>
2026-09-02 21:18:29 +0300

    update: add spa redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import spaRedirect from 'vite-plugin-spa-redirect'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    spaRedirect()
+  ],
   base: '/weather-frontend-app/',
 })
